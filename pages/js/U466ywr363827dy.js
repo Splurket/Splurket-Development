879018c2758e438788a36ff2248f44e88e57a7e9
Dylan Clayton <dylanclayton3@gmail.com>
2021-05-13 12:56:43 -0400

    commit

--- pages/js/U466ywr363827dy.js
+++ pages/js/U466ywr363827dy.js
@@ -1,42 +1,38 @@
 var product_items;
 var catshit;
-var product_data = [];
+var user_data = [];
 var user;
 var email1;
-     db.collection("products").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-
-            if (doc.exists) {
-              var doc= doc.data();
-              catshit = doc.product_category;
-              var push_data1 = {value: false, id: `${doc.product_id}`, name:`${doc.product_name}`, price: `${doc.product_price}`, date: `${doc.creation_date}`, reviewsn: `${doc.product_reviewsn}`, creator: `${doc.product_creator}`, image: `${doc.product_cover}`, creatorpic: `${doc.product_creatorpic}`, desc: `${doc.product_description}`}
-              if (product_data.includes('{')){
-                var push_data = ','+push_data1;
-                product_data.push(push_data)
-
-
-              }else{
-                var push_data = push_data1;
-                product_data.push(push_data)
-
-              }//document.write(product_data)
-
-
-
-
-            } else {
-                // doc.data() will be undefined in this case
-                product_data=[{
-                  value: false,
-                  name: 'No Products Found',
-                  Price: 'N/A',
-                  reviews: 'N/A',
-                  status: 'N/A' }]
-            }
-          })
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
+function getuserprodata(prodata){
+  var docRef = db.collection('pubusers').doc(prodata);
+    docRef.get().then((doc) => {
+     
+                 if (doc.exists) {
+                   var doc= doc.data();
+                   var push_data1 = {value: false, name: `${doc.username}`}
+                   if (user_data.includes('{')){
+                     var push_data = ','+push_data1;
+                     user.push(push_data)
+     
+     
+                   }else{
+                     user_data.push(push_data1)
+     
+                   }//document.write(product_data)
+     
+     
+     
+     
+                 } else {
+                     // doc.data() will be undefined in this case
+                     user_data=[{
+                       value: false,
+                       name: 'No User Found'}]
+                 }
+             }).catch((error) => {
+                 console.log("Error getting document:", error);
+             });
+           }
 new Vue({
   el: '#app',
   vuetify: new Vuetify(),
@@ -61,7 +57,7 @@ new Vue({
       ],
 
 
-      items: product_data};},
+      items: user_data};},
   computed: {
     numberOfPages() {
       return Math.ceil(this.items.length / this.itemsPerPage);
