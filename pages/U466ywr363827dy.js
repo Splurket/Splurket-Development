30cc219756db3e6dc9061d1b9cfa99715481cd17
Dylan Clayton <dylanclayton3@gmail.com>
2021-05-13 16:19:51 -0400

    commit

--- pages/U466ywr363827dy.js
+++ pages/U466ywr363827dy.js
@@ -18,6 +18,111 @@ var config = {
 firebase.initializeApp(config);
 const db = firebase.firestore();
 db.settings({ timestampsInSnapshots: true }); 
+function getParameterByName(name, url = window.location.href) {
+            name = name.replace(/[\[\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+        var userfile= getParameterByName('user');
+        getuserprodata(userfile);
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            var user = firebase.auth().currentUser;
+            //splurket@gmail.com
+            var email1 = user.email;
+            
+            /*var email = getParameterByName('user'); 
+            console.log(email)*/
+            //still splurket
+            
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Accept", "application/json, /;q=0.5");
+
+            var raw = JSON.stringify({
+              "stringtoencrypt": `${email1}`
+            });
+            //document.write(raw)
+            var requestOptions = {
+              method: 'POST',
+              headers: myHeaders,
+              body: raw,
+              redirect: 'follow'
+            };
+
+            fetch("https://americanrivergold.com/fluffybunnyin", requestOptions)
+              .then(function (response) {
+                  return response.json();
+              })
+              .then(function (data) {
+                //should be a base64 string by now
+                getuserdata(data);
+            });
+          } else {
+            console.log('fuck')
+            document.getElementById("avatar").style.display = "none";
+            document.getElementById("login").style.display = "block";
+            document.getElementById('cartbutton').style.top='20px';
+            var all = document.getElementsByClassName('headerdrop');
+            for (var i = 0; i < all.length; i++) {
+              all[i].style.top = '83px';
+            }
+          }
+        });
+        function getuserdata(data) {
+            email = data.encryptedstring;
+                var docRef = db.collection("users").doc(email);
+                docRef.get().then((doc) => {
+                    if (doc.exists) {
+                        data1 = doc.data()
+                        pro_pic = data1.pro_pic;
+                        if (data1.username == "" ){
+                            user_name = data1.email
+                        }
+                        if (data1.username != "") {
+                            user_name = data1.username;
+                        }
+                        document.getElementById("avatarpic").src = pro_pic;
+                        //document.getElementById("avatarpic1").src = pro_pic;
+                        document.getElementById("avatarusername1").innerText = "Signed In As: " + user_name;
+                        document.getElementById("avatar").style.display = "block";
+                        document.getElementById("avatarpic").style.display = "inline-block";
+                        var all = document.getElementsByClassName('headerdrop');
+                        for (var i = 0; i < all.length; i++) {
+                          all[i].style.top = '98px';
+                        }
+                    } else {
+                        // doc.data() will be undefined in this case
+                        console.log("No such document!");
+                        console.log(doc.data())
+                    }
+                }).catch((error) => {
+                    console.log("Error getting document:", error);
+                });
+        }
+        function profilego() {
+            var localstuff1 = localStorage.getItem('usercreds');
+            var localstuff = JSON.parse(localstuff1)
+            var loginemail = btoa(localstuff.email);
+            var loginpassword = btoa(localstuff.password);
+            location.href = "https://myaccount.splurket.com/login?bunnyluck="+loginemail+"&rabbitfoot="+loginpassword
+
+        }
+        function signout() {
+            firebase.auth().signOut().then(() => {
+                localStorage.removeItem('usercreds');
+                var all = document.getElementsByClassName('headerdrop');
+                for (var i = 0; i < all.length; i++) {
+                  all[i].style.top = '83px';
+                }
+            }).catch((error) => {
+              // An error happened.
+            });
+        }
 function getuserprodata(prodata){
   var docRef = db.collection('pubusers').doc(prodata);
     docRef.get().then((doc) => {
