e1001af695d511903e2c777de51206b43bc6c8c3
Dylan Clayton <dylanclayton3@gmail.com>
2021-05-27 16:45:54 -0400

    commit

--- arcflashengine.js
+++ arcflashengine.js
@@ -1,7 +1,7 @@
 // -------------------------------------------
 // DEFAULT INPUT AND OUTPUT AREA
 textarea = document.querySelector( '#input-area' );
-textarea.value= "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n [Herman Fassett](https://freecodecamp.com/hermanfassett) \n\n![Image Name](https://i.imgur.com/AaslDzA.png)"
+textarea.value= "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n [Splurket Marketplace](https://splurket.com) \n\n![Image Name](https://i.imgur.com/AaslDzA.png)"
 let outputArea = document.querySelector( '#output-area' );
 let previewMessage = document.querySelector( '.preview-message' );
 
