d10d4010a83e6863755eaa8d86849cd4f0956a40
Dylan Clayton <dylanclayton3@gmail.com>
2021-05-31 16:42:42 -0400

    commit

--- cart/cart.js
+++ cart/cart.js
@@ -1,14 +1,13 @@
-    const items1=[];
+const items=[];
   var cart = getCart();
   for(var i = 0; i < cart.length; i++) {
     var cartItem = cart[i];
     pushdata= {value: false, id: `${cartItem.data.id}`, name: `${cartItem.data.description}`, quantity: `${cartItem.quantity}`, price: `${cartItem.data.price}`}
-    items1.push(pushdata)
+    items.push(pushdata)
 
     console.log(cartItem.data.id);
-    console.log(items1)
-    console.log(cart[i])
   }
+  console.log(items)
   populateCart();
   function populateCart() {
 
