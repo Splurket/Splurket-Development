fbdd4737c7f7e41e73eea32fe4054784ee6504b5
Dylan Clayton <dylanclayton3@gmail.com>
2021-06-12 20:42:53 -0400

    commit

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,16 @@
-//Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/8.6.7/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.6.7/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+// importScripts('/__/firebase/init.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
+// Initialize the Firebase app in the service worker by passing in the
+// messagingSenderId.
 firebase.initializeApp({
-  apiKey: 'api-key',
-  authDomain: 'project-id.firebaseapp.com',
-  databaseURL: 'https://project-id.firebaseio.com',
-  projectId: 'project-id',
-  storageBucket: 'project-id.appspot.com',
-  messagingSenderId: 'sender-id',
-  appId: 'app-id',
-  measurementId: 'G-measurement-id',
+'messagingSenderId': '260798041309'
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
+firebase.messaging();
