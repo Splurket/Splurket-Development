0198f0b6f97040afd6052837b96344595f312f83
DillonMercz <dylanclayton3@gmail.com>
2021-09-18 21:18:38 -0400

    HTML editor update

---
+++ index.js
@@ -0,0 +1,39 @@
+const { exec } = require("child_process");
+exec("git add .", (error, data, getter) => {
+	if(error){
+		console.log("error",error.message);
+		return;
+	}
+	if(getter){
+		console.log("data",data);
+		return;
+	}
+	console.log("data",data);
+
+});
+
+exec('git commit -m "commit"', (error, data, getter) => {
+	if(error){
+		console.log("error",error.message);
+		return;
+	}
+	if(getter){
+		console.log("data",data);
+		return;
+	}
+	console.log("data",data);
+
+});
+
+exec("git push", (error, data, getter) => {
+	if(error){
+		console.log("error",error.message);
+		return;
+	}
+	if(getter){
+		console.log("data",data);
+		return;
+	}
+	console.log("data",data);
+
+});
